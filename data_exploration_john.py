d145b94f280fb613c7e87718248ccccaf4c653d3
jjo21 <jjo21@ic.ac.uk>
2025-03-21 11:26:21 +0000

    reading data

---
+++ data_exploration_john.py
@@ -0,0 +1,13 @@
+#%%
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#%%
+
+avp = pd.read_csv('2023\Ancillary Volumes & Prices (4H).csv')
+dap = pd.read_csv('2023\Day-Ahead Price (1H).csv')
+paf = pd.read_csv('2023\Prices & Forecasts (HH).csv')
+
+# %%
